47c60cfea74c919917fada4d287043cffd4273f4
Sridharan Lakshmanan <sridharan.lakshmanan@iMac.local>
2026-04-11 14:06:02 +0300

    3 digit combos in ascending order

--- printcomb.go
+++ printcomb.go
@@ -1,14 +1,16 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func PrintComb() {
-	for i := 0; i <= 7; i++ {
-		for j := i + 1; j <= 8; j++ {
-			for k := j + 1; k <= 9; k++ {
-				z01.PrintRune(rune(i) + '0')
-				z01.PrintRune(rune(j) + '0')
-				z01.PrintRune(rune(k) + '0')
+	for i := '0'; i <= '7'; i++ {
+		for j := i + 1; j <= '8'; j++ {
+			for k := j + 1; k <= '9'; k++ {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
 
 				if i != 7 || j != 8 || k != 9 { // comma space not needed at the last
 					z01.PrintRune(',')
