6579d873866cd221dfce9d2b3d393af1dc9bcae9
Sridharan Lakshmanan <sridharan.lakshmanan@iMac.local>
2026-04-11 14:02:57 +0300

    3 digit combos in ascending order

--- printcomb.go
+++ printcomb.go
@@ -4,20 +4,17 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for i := 0; i <= 7; i++ {
-
 		for j := i + 1; j <= 8; j++ {
-
 			for k := j + 1; k <= 9; k++ {
 				z01.PrintRune(rune(i) + '0')
 				z01.PrintRune(rune(j) + '0')
 				z01.PrintRune(rune(k) + '0')
 
-				if i != 7 || j != 8 || k != 9 { //comma space not needed at the last
+				if i != 7 || j != 8 || k != 9 { // comma space not needed at the last
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				}
 			}
 		}
-
 	}
 }
