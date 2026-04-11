39357781f1f15c21a36a52369a044451ccc95e31
Sridharan Lakshmanan <sridharan.lakshmanan@iMac.local>
2026-04-11 14:54:53 +0300

    diff 2 digit combinations in ascending order

--- printcomb2.go
+++ printcomb2.go
@@ -7,17 +7,18 @@ import (
 func PrintComb2() {
 	for a := '0'; a <= '9'; a++ {
 		for b := a; b <= '9'; b++ {
-			z01.PrintRune(a)
-			z01.PrintRune(b)
-			z01.PrintRune(' ')
 			for i := '0'; i <= '8'; i++ {
 				for j := i + 1; j <= '9'; j++ {
 					if i != j {
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(' ')
 						z01.PrintRune(i)
 						z01.PrintRune(j)
 						if i != '8' { // comma space not needed at the last
 							z01.PrintRune(',')
 							z01.PrintRune(' ')
+						}
 					}
 				}
 			}
