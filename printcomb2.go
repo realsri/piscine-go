4fc20682e516c5973c6b835e27e944be98fe58d6
Sridharan Lakshmanan <sridharan.lakshmanan@iMac.local>
2026-04-11 14:19:26 +0300

    diff 2 digit combinations in ascending order

---
+++ printcomb2.go
@@ -0,0 +1,23 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintComb2() {
+	for i := '0'; i <= '8'; i++ {
+		for j := i + 1; j <= '9'; j++ {
+
+			if i != j {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				if i != '8' { // comma space not needed at the last
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				} else {
+					z01.PrintRune('\n')
+				}
+			}
+		}
+	}
+}
