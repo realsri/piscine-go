805e71b4625d183337e76bd12765d7f557733d29
Sridharan Lakshmanan <sridharan.lakshmanan@iMac.local>
2026-04-12 13:31:01 +0300

    count the runes

---
+++ strlen.go
@@ -0,0 +1,23 @@
+// package main
+package piscine
+
+/*
+import (
+	"fmt"
+)
+*/
+
+func StrLen(s string) int {
+	count := 0
+	for range s {
+		count++
+	}
+	return count
+}
+
+/*
+func main() {
+	l := StrLen("Hello World!")
+	fmt.Println(l)
+}
+*/
