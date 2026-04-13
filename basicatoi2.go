f7bf50b11203111039daaaad6b2d2d621b07758d
Sridharan Lakshmanan <sridharan.lakshmanan@iMac.local>
2026-04-13 20:29:30 +0300

    ascii to int with other char check

---
+++ basicatoi2.go
@@ -0,0 +1,29 @@
+//package main
+
+//import "fmt"
+
+package piscine
+
+func BasicAtoi2(s string) int {
+	// str := []rune(s)
+	result := 0
+	for _, char := range s {
+		if char < '0' || char > '9' {
+			return 0
+		}
+		digit := int(char - '0')
+		result = result*10 + digit
+	}
+
+	return result
+}
+
+/*
+func main() {
+	fmt.Println(BasicAtoi2("12345"))
+	fmt.Println(BasicAtoi2("0000000012345"))
+	fmt.Println(BasicAtoi2("000000"))
+	fmt.Println(BasicAtoi2("012 345"))
+	fmt.Println(BasicAtoi2("Hello World!"))
+}
+*/
