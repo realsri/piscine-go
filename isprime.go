49ac1d2727cc084536ff258ef278f25915b01e9e
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-16 01:10:22 +0300

    is it prime

--- isprime.go
+++ isprime.go
@@ -3,18 +3,19 @@
 // import "fmt"
 package piscine
 
-func Sqrt1(nb int) int {
-	if nb == 0 {
+/*
+	func Sqrt1(nb int) int {
+		if nb == 0 {
+			return 0
+		}
+		for i := 0; i <= nb; i++ {
+			if nb == i*i {
+				return i
+			}
+		}
 		return 0
 	}
-	for i := 0; i <= nb; i++ {
-		if nb == i*i {
-			return i
-		}
-	}
-	return 0
-}
-
+*/
 func IsPrime(nb int) bool {
 	if nb <= 1 {
 		return false
@@ -22,7 +23,7 @@ func IsPrime(nb int) bool {
 	if nb%2 == 0 {
 		return false
 	}
-	for i := 2; i <= Sqrt1(nb); i++ {
+	for i := 2; i <= nb; i++ {
 		if nb%i == 0 {
 			return false
 		}
