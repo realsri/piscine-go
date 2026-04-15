c7ed046c6f0ca9f958d6249504b50d45635ffa0d
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-16 00:41:58 +0300

    is it prime

---
+++ isprime.go
@@ -0,0 +1,26 @@
+// package main
+package piscine
+
+/*
+import (
+	"fmt"
+)*/
+
+func IsPrime(nb int) bool {
+	if nb < 2 {
+		return false
+	}
+	for i := 2; i < nb; i++ {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+/*
+func main() {
+	fmt.Println(IsPrime(5))
+	fmt.Println(IsPrime(4))
+}
+*/
