4d997dd29a347278f50e5af67e3fac89b01c648d
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-15 23:16:35 +0300

    fibonacci

---
+++ fibonacci.go
@@ -0,0 +1,22 @@
+//package main
+
+package piscine
+
+func Fibonacci(index int) int {
+	if index < 0 {
+		return -1
+	}
+	if index == 0 {
+		return 0
+	}
+	if index == 1 {
+		return 1
+	}
+	return Fibonacci(index-1) + Fibonacci(index-2)
+}
+
+/*
+func main() {
+	arg1 := 4
+	fmt.Println(Fibonacci(arg1))
+}*/
