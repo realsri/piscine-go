9394cd34d1d6c3b5d9a51abb8647b9e95b754e4b
Sridharan Lakshmanan <sridharan.lakshmanan@iMac.local>
2026-04-15 15:26:16 +0300

    factorial program

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,9 +1,8 @@
 // package main
-
-// import "fmt"
 package piscine
 
-func IterativeFactorial(nb int) int {
+/*
+func IterativeFactorial(nb int) int { //oops! it is recursive factorial. not so memory efficient but clean //human comment not ai
 	if nb == 0 {
 		return 1
 	} else if nb < 0 {
@@ -11,9 +10,21 @@ func IterativeFactorial(nb int) int {
 	} else {
 		return IterativeFactorial(nb-1) * nb
 	}
+}*/
+
+func IterativeFactorial(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+	result := 1
+	for i := 1; i <= nb; i++ {
+		result = result * i
+	}
+	return result
 }
 
 /*
+
 func main() {
 	arg := 4
 	fmt.Println(IterativeFactorial(arg))
