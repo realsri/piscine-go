cc9055b02e38a7b9c49f1a3965ab45663bbadc1f
Sridharan Lakshmanan <sridharan.lakshmanan@iMac.local>
2026-04-15 14:55:10 +0300

    factorial program

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,8 +4,10 @@ package piscine
 func IterativeFactorial(nb int) int {
 	if nb > 0 {
 		return IterativeFactorial(nb-1) * nb
-	} else {
+	} else if nb == 0 {
 		return 1
+	} else {
+		return 0
 	}
 }
 
