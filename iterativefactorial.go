ecdc905808a9339a2c1c765f57546458f12ad647
Sridharan Lakshmanan <sridharan.lakshmanan@iMac.local>
2026-04-15 14:59:54 +0300

    factorial program

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,18 +1,19 @@
-// package main
-package piscine
+package main
 
+import "fmt"
+
+// package piscine
 func IterativeFactorial(nb int) int {
-	if nb > 0 {
-		return IterativeFactorial(nb-1) * nb
-	} else if nb == 0 {
+	if nb == 0 {
 		return 1
-	} else {
+	} else if nb < 0 {
 		return 0
+	} else {
+		return IterativeFactorial(nb-1) * nb
 	}
 }
 
-/*
 func main() {
 	arg := 4
 	fmt.Println(IterativeFactorial(arg))
-}*/
+}
