65f4f53461a1eebe3d02a499f7ac27a6491b79ed
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-16 01:35:34 +0300

    find prime or next prime

---
+++ findnextprime.go
@@ -0,0 +1,40 @@
+package main
+
+// package piscine
+
+import "fmt"
+
+func IsPrime(nb int) bool {
+	if nb <= 1 {
+		return false
+	}
+	if nb == 2 {
+		return true
+	}
+	if nb%2 == 0 {
+		return false
+	}
+	for i := 3; i*i <= nb; i += 2 {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func FindNextPrime(nb int) int {
+	if nb <= 2 {
+		return 2
+	}
+	for i := nb; true; i++ {
+		if IsPrime(i) {
+			return i
+		}
+	}
+	return 0
+}
+
+func main() {
+	fmt.Println(FindNextPrime(-1))
+	fmt.Println(FindNextPrime(2))
+}
