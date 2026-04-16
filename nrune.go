9f065c48347e57bf35400a17c7d0c5df571bd8ca
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-16 17:01:10 +0300

    nth rune i.e (n-1)th index

---
+++ nrune.go
@@ -0,0 +1,27 @@
+// package main
+package piscine
+
+/*
+import (
+	"github.com/01-edu/z01"
+)*/
+
+func NRune(s string, n int) rune {
+	srune := []rune(s)
+	if n <= len(srune) && n > 0 {
+		return srune[n-1]
+	} else {
+		return 0
+	}
+}
+
+/*
+func main() {
+	z01.PrintRune(NRune("Hello!", 3))
+	z01.PrintRune(NRune("Salut!", 2))
+	z01.PrintRune(NRune("Bye!", -1))
+	z01.PrintRune(NRune("Bye!", 5))
+	z01.PrintRune(NRune("Ola!", 4))
+	z01.PrintRune('\n')
+}
+*/
