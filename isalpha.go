ae523be50be8803e3d62d1570eee0d3826f2cd0d
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-17 16:22:32 +0300

    Are all alphas or nums?

--- isalpha.go
+++ isalpha.go
@@ -13,7 +13,7 @@ func IsAlpha(s string) bool {
 		return false
 	}
 	for _, c := range s {
-		if !((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+		if !((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= 0 && c <= 9)) {
 			return false
 		}
 	}
