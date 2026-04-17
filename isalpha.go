d2a3e9d018bdafe7b9b11e00a32a8f31545c5bfb
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-17 16:20:14 +0300

    Are all alphas?

---
+++ isalpha.go
@@ -0,0 +1,29 @@
+// package main
+package piscine
+
+/*
+import (
+	"fmt"
+	//"piscine"
+)
+*/
+
+func IsAlpha(s string) bool {
+	if len(s) < 1 {
+		return false
+	}
+	for _, c := range s {
+		if !((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+			return false
+		}
+	}
+	return true
+}
+
+/*
+func main() {
+	fmt.Println(IsAlpha("Hello! How are you?"))
+	fmt.Println(IsAlpha("HelloHowareyou"))
+	fmt.Println(IsAlpha("What's this 4?"))
+	fmt.Println(IsAlpha("Whatsthis4"))
+}*/
