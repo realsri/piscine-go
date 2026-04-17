84572d48c626ea7c1984e0c17b72ab7bda58d5c6
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-17 17:04:05 +0300

    does it contain all printable chars

---
+++ isprintable.go
@@ -0,0 +1,28 @@
+package main
+
+//package piscine
+/*
+import (
+	"fmt"
+	//"piscine"
+)*/
+
+func IsPrintable(s string) bool {
+	if len(s) < 1 {
+		return false
+	}
+	for _, c := range s {
+		if !(c >= 32 && c <= 126) {
+			return false
+		}
+	}
+	return true
+}
+
+/*
+func main() {
+	fmt.Println(IsPrintable("Hello"))
+	fmt.Println(IsPrintable("Hello\n"))
+
+}
+*/
