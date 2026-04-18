a22e0b963bd33e469775f212ca30898ff4939f1e
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-18 16:15:55 +0300

    print program name without ./

---
+++ printprogramname/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args
+
+	arg0 := args[0]
+
+	len := len(arg0)
+
+	for i := 2; i < len; i++ {
+		z01.PrintRune(rune(arg0[i]))
+	}
+}
