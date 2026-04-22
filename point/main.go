3ed0c400d02ba8743bba590b43cfee0abb47d663
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-22 20:02:46 +0300

    hard but possible I think without numbers and limited PrintRunes

---
+++ point/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+type point struct {
+	x int
+	y int
+}
+
+func setPoint(x *int, y *int) {
+	*x = '*'
+	*y = '*' / 2
+}
+
+func printNumber(n int) {
+	if n >= 10 {
+		printNumber(n / 10)
+	}
+	z01.PrintRune(rune(n%10 + '0'))
+}
+
+func printStr(s string) {
+	for _, c := range s {
+		z01.PrintRune(c)
+	}
+}
+
+func main() {
+	points := &point{}
+
+	setPoint(&points.x, &points.y)
+
+	printStr("x = ")
+	printNumber(points.x)
+	printStr(", y = ")
+	printNumber(points.y)
+	z01.PrintRune('\n')
+
+	// fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+}
