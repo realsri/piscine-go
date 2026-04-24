8739281ef938c88c3408926871397d9d1ea4aa18
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-24 15:22:33 +0300

    checks if the int array is sorted or not

---
+++ issorted.go
@@ -0,0 +1,31 @@
+package piscine
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+func f(a, b int) int {
+	return a - b
+}
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	for i := 0; i < len(a)-1; i++ {
+		if f(a[i], a[i+1]) > 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// func main() {
+// 	a1 := []int{0, 1, 2, 3, 4, 5}
+// 	a2 := []int{0, 2, 1, 3}
+
+// 	result1 := IsSorted(f, a1)
+// 	result2 := IsSorted(f, a2)
+
+// 	fmt.Println(result1)
+// 	fmt.Println(result2)
+// }
