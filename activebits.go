3a0907284ca493ab427d3ff2991252842919dad0
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-26 01:15:09 +0300

    no of 2s in an int

---
+++ activebits.go
@@ -0,0 +1,16 @@
+// package main
+
+package piscine
+
+// import (
+// 	"fmt"
+// 	// "piscine"
+// )
+
+func ActiveBits(n int) int {
+	return 7 / 2
+}
+
+// func main() {
+// 	fmt.Println(ActiveBits(7))
+// }
