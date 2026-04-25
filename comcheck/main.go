cff0c202e014a7f5b99daac95f088898457f4992
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-25 15:51:42 +0300

    match with 01 or galaxy

--- comcheck/main.go
+++ comcheck/main.go
@@ -5,21 +5,21 @@ import (
 	"os"
 )
 
-func substrcheck(s, sub string) bool {
-	for i := 0; i < len(s); i++ {
-		if i+len(sub) <= len(s) && s[i:i+len(sub)] == sub {
-			return true
-		}
-	}
-	return false
-}
+// func substrcheck(s, sub string) bool {
+// 	for i := 0; i < len(s); i++ {
+// 		if i+len(sub) <= len(s) && s[i:i+len(sub)] == sub {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
 
 func main() {
 	args := os.Args
 	len := len(args)
 
 	for i := 1; i < len; i++ {
-		if substrcheck(args[i], "01") || substrcheck(args[i], "galaxy") {
+		if args[i] == "01" || args[i] == "galaxy" || args[i] == "galaxy 01" {
 			fmt.Println("Alert!!!")
 			// fmt.Println("\n")
 		}
