e7f271777f25c03b761e5c07693ca4edf5b14cd8
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-25 15:44:20 +0300

    match with 01 or galaxy

---
+++ comcheck/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func substrcheck(s, sub string) bool {
+	for i := 0; i < len(s); i++ {
+		if i+len(sub) <= len(s) && s[i:i+len(sub)] == sub {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	args := os.Args
+	len := len(args)
+
+	for i := 1; i < len; i++ {
+		if substrcheck(args[i], "01") || substrcheck(args[i], "galaxy") {
+			fmt.Println("Alert!!!")
+			//fmt.Println("\n")
+		}
+	}
+
+}
