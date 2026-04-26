ae009e03e00cd614ca3adeb3ba0f581d609b1af1
Sridharan Lakshmanan <sridharan.lakshmanan@iMac-2.local>
2026-04-26 05:48:26 +0300

    find the first lonely one

---
+++ unmatch.go
@@ -0,0 +1,30 @@
+package piscine
+
+// package main
+
+// import (
+// 	"fmt"
+// 	// "piscine"
+// )
+
+func Unmatch(a []int) int {
+	len := len(a)
+	for i := 0; i < len; i++ {
+		count := 0
+		for j := 0; j < len; j++ {
+			if a[i] == a[j] {
+				count++
+			}
+		}
+		if count == 1 {
+			return a[i]
+		}
+	}
+	return -1
+}
+
+// func main() {
+// 	a := []int{1, 2, 3, 1, 2, 3, 4}
+// 	unmatch := Unmatch(a)
+// 	fmt.Println(unmatch)
+// }
