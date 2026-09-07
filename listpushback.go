815c93d7590c0d3df16890c76972be6b7b47e768
Sridharan L <sridharan.lakshmanan@gritlab.ax>
2026-09-07 17:10:38 +0300

    singly linked list - insert at the tail

---
+++ listpushback.go
@@ -0,0 +1,48 @@
+package piscine
+
+/*
+import (
+
+	"fmt"
+	//"piscine"
+
+)
+*/
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+
+	n := &NodeL{data, nil}
+
+	if l.Head == nil {
+		l.Head = n
+		l.Tail = n
+	} else {
+		l.Tail.Next = n
+		l.Tail = n
+	}
+}
+
+/*
+func main() {
+
+	link := &List{}
+
+	ListPushBack(link, "Hello")
+	ListPushBack(link, "man")
+	ListPushBack(link, "how are you")
+
+	for link.Head != nil {
+		fmt.Println(link.Head.Data)
+		link.Head = link.Head.Next
+	}
+}
+*/
