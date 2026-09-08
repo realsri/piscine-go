6a157b10709fe5c74c25da663a9a623bee19f546
Sridharan L <sridharan.lakshmanan@gritlab.ax>
2026-09-08 18:10:08 +0300

    singly linked list - clear the list

---
+++ listclear.go
@@ -0,0 +1,63 @@
+package piscine
+
+/*
+package main
+
+import (
+
+	"fmt"
+
+)
+
+	type NodeL struct {
+		Data interface{}
+		Next *NodeL
+	}
+
+	type List struct {
+		Head *NodeL
+		Tail *NodeL
+	}
+
+	func ListPushBack(l *List, data interface{}) {
+		n := &NodeL{data, nil}
+
+		if l.Head == nil {
+			l.Head = n
+			l.Tail = n
+		} else {
+			l.Tail.Next = n
+			l.Tail = n
+		}
+	}
+
+	func PrintList(l *List) {
+		link := l.Head
+		for link != nil {
+			fmt.Print(link.Data, " -> ")
+			link = link.Next
+		}
+		fmt.Println(nil)
+	}
+*/
+func ListClear(l *List) {
+	l.Head = nil
+	l.Tail = nil
+}
+
+/*
+func main() {
+	link := &List{}
+
+	ListPushBack(link, "I")
+	ListPushBack(link, 1)
+	ListPushBack(link, "something")
+	ListPushBack(link, 2)
+
+	fmt.Println("------list------")
+	PrintList(link)
+	ListClear(link)
+	fmt.Println("------updated list------")
+	PrintList(link)
+}
+*/
