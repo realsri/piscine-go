5b067bfd4f871831b17ea144abb893f9c1c08407
Sri <83589407+realsri@users.noreply.github.com>
2026-09-09 22:46:48 +0300

    Binary Search Tree - function apply by levels

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,38 +1,43 @@
+package piscine
+
+/*
 package main
 
 import (
+
 	"fmt"
+
 )
 
-func main() {
-	root := &TreeNode{Data: "4"}
-	BTreeInsertData(root, "1")
-	BTreeInsertData(root, "7")
-	BTreeInsertData(root, "5")
-	BTreeApplyByLevel(root, fmt.Println)
-}
-
-type TreeNode struct {
-	Left, Right, Parent *TreeNode
-	Data                string
-}
-
-func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-	if root == nil {
-		return &TreeNode{Data: data}
+	func main() {
+		root := &TreeNode{Data: "4"}
+		BTreeInsertData(root, "1")
+		BTreeInsertData(root, "7")
+		BTreeInsertData(root, "5")
+		BTreeApplyByLevel(root, fmt.Println)
 	}
 
-	if data < root.Data {
-		root.Left = BTreeInsertData(root.Left, data)
-		root.Left.Parent = root
-	} else {
-		root.Right = BTreeInsertData(root.Right, data)
-		root.Right.Parent = root
+	type TreeNode struct {
+		Left, Right, Parent *TreeNode
+		Data                string
 	}
 
-	return root
-}
+	func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+		if root == nil {
+			return &TreeNode{Data: data}
+		}
 
+		if data < root.Data {
+			root.Left = BTreeInsertData(root.Left, data)
+			root.Left.Parent = root
+		} else {
+			root.Right = BTreeInsertData(root.Right, data)
+			root.Right.Parent = root
+		}
+
+		return root
+	}
+*/
 func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
