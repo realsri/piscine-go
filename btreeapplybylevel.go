f52b82cc34d8bc07d659d9f438730557e8e3cb3f
Sri <83589407+realsri@users.noreply.github.com>
2026-09-09 22:46:10 +0300

    Binary Search Tree - function apply by levels

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,49 +1,57 @@
-package piscine
-
-/*
 package main
 
 import (
-
 	"fmt"
-
 )
 
-	func main() {
-		root := &TreeNode{Data: "4"}
-		BTreeInsertData(root, "1")
-		BTreeInsertData(root, "7")
-		BTreeInsertData(root, "5")
-		BTreeApplyByLevel(root, fmt.Println)
+func main() {
+	root := &TreeNode{Data: "4"}
+	BTreeInsertData(root, "1")
+	BTreeInsertData(root, "7")
+	BTreeInsertData(root, "5")
+	BTreeApplyByLevel(root, fmt.Println)
+}
+
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
 	}
 
-	type TreeNode struct {
-		Left, Right, Parent *TreeNode
-		Data                string
+	if data < root.Data {
+		root.Left = BTreeInsertData(root.Left, data)
+		root.Left.Parent = root
+	} else {
+		root.Right = BTreeInsertData(root.Right, data)
+		root.Right.Parent = root
 	}
 
-	func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-		if root == nil {
-			return &TreeNode{Data: data}
-		}
+	return root
+}
 
-		if data < root.Data {
-			root.Left = BTreeInsertData(root.Left, data)
-			root.Left.Parent = root
-		} else {
-			root.Right = BTreeInsertData(root.Right, data)
-			root.Right.Parent = root
-		}
-
-		return root
-	}
-*/
 func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-
-	f(root.Data)
-	BTreeApplyByLevel(root.Left, f)
-	BTreeApplyByLevel(root.Right, f)
+	/*
+		f(root.Data)
+		BTreeApplyByLevel(root.Left, f)
+		BTreeApplyByLevel(root.Right, f)
+	*/
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+		f(node.Data)
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+	}
 }
