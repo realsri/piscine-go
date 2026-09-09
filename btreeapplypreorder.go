04edeaeb328d43fd69edb74d3ab2ab784f0ceb7e
Sri <83589407+realsri@users.noreply.github.com>
2026-09-09 20:54:38 +0300

    Binary Search Tree - preorder traversal with a function

---
+++ btreeapplypreorder.go
@@ -0,0 +1,50 @@
+package piscine
+
+/*
+package main
+
+import (
+
+	"fmt"
+
+)
+
+	func main() {
+		root := &TreeNode{Data: "4"}
+		BTreeInsertData(root, "1")
+		BTreeInsertData(root, "7")
+		BTreeInsertData(root, "5")
+		BTreeApplyPreorder(root, fmt.Println)
+
+}
+
+	type TreeNode struct {
+		Left, Right, Parent *TreeNode
+		Data                string
+	}
+
+	func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+		if root == nil {
+			return &TreeNode{Data: data}
+		}
+
+		if data < root.Data {
+			root.Left = BTreeInsertData(root.Left, data)
+			root.Left.Parent = root
+		} else {
+			root.Right = BTreeInsertData(root.Right, data)
+			root.Right.Parent = root
+		}
+
+		return root
+	}
+*/
+func BTreeApplyPreorder(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+
+	f(root.Data)
+	BTreeApplyPreorder(root.Left, f)
+	BTreeApplyPreorder(root.Right, f)
+}
