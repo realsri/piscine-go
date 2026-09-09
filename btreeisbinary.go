63e27b8e00aeb206c6e322169335e1e5405d5167
Sri <83589407+realsri@users.noreply.github.com>
2026-09-09 22:24:56 +0300

    Binary Search Tree - BST check

--- btreeisbinary.go
+++ btreeisbinary.go
@@ -1,38 +1,43 @@
+package piscine
+
+/*
 package main
 
 import (
+
 	"fmt"
+
 )
 
-func main() {
-	root := &TreeNode{Data: "4"}
-	BTreeInsertData(root, "1")
-	BTreeInsertData(root, "7")
-	BTreeInsertData(root, "5")
-	fmt.Println(BTreeIsBinary(root))
-}
-
-type TreeNode struct {
-	Left, Right, Parent *TreeNode
-	Data                string
-}
-
-func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-	if root == nil {
-		return &TreeNode{Data: data}
+	func main() {
+		root := &TreeNode{Data: "4"}
+		BTreeInsertData(root, "1")
+		BTreeInsertData(root, "7")
+		BTreeInsertData(root, "5")
+		fmt.Println(BTreeIsBinary(root))
 	}
 
-	if data < root.Data {
-		root.Left = BTreeInsertData(root.Left, data)
-		root.Left.Parent = root
-	} else {
-		root.Right = BTreeInsertData(root.Right, data)
-		root.Right.Parent = root
+	type TreeNode struct {
+		Left, Right, Parent *TreeNode
+		Data                string
 	}
 
-	return root
-}
+	func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+		if root == nil {
+			return &TreeNode{Data: data}
+		}
 
+		if data < root.Data {
+			root.Left = BTreeInsertData(root.Left, data)
+			root.Left.Parent = root
+		} else {
+			root.Right = BTreeInsertData(root.Right, data)
+			root.Right.Parent = root
+		}
+
+		return root
+	}
+*/
 func BTreeIsBinary(root *TreeNode) bool {
 	if root == nil {
 		return true
